adad30e503d104f355ab3d6e786e600a5f4faf65
Sundar <91257478+Sundarleo11@users.noreply.github.com>
2022-05-31 09:19:06 +0530

    Update AuthorisingClient.java

--- src/main/java/com/pension/process/feignclient/AuthorisingClient.java
+++ src/main/java/com/pension/process/feignclient/AuthorisingClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 //@FeignClient(name = "Authorizatiion-Microservice", url = "http://localhost:8000/auth/api")
-@FeignClient(name = "Authorizatiion-Microservice", url = "http://pms-prod-auth-lb-1622755753.us-east-1.elb.amazonaws.com/auth/api")
+@FeignClient(name = "Authorizatiion-Microservice", url = "http://pms-prod-auth-lb-572975.us-east-1.elb.amazonaws.com/auth/api")
 public interface AuthorisingClient {
 
 	@PostMapping("/authorize")
